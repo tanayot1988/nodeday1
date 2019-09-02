6c307e77d945204226ecde1c764c689ea2a7187a
tanayot <suwarod1@gmail.com>
2019-09-02 15:06:06 +0700

    version1

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const http = require('http')
+
+
+const server = http.createServer((req, res) => {
+    const url = req.url
+
+switch(url){
+    case '/':
+        res.end('<h1>Home Page</h1>')
+        break;
+    case '/about':
+        res.end('<h1>About Page</h1>')
+        break;
+
+    default:
+           res.end('<h1>Page Not Found</h1>')
+        break;
+}
+
+
+    console.log(url)
+    
+    res.end('<h1>Hello Tanayot OAT</h1>')
+})
+
+const port = process.env.port
+server.listen(port)
+
